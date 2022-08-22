c57fe3ae10f1959573ae75518b07f157f3629915
dansmith09 <dansmith_09@hotmail.com>
2022-08-22 23:17:44 +1000

    added alerts for user error on signup and login forms

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 // Modified code from Module 14 mini project
 const loginFormHandler = async (event) => {
   event.preventDefault();
-
+  hideLoginAlert();
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
@@ -19,17 +19,20 @@ const loginFormHandler = async (event) => {
       // This may need to be changed depending upon what we choose the user main page to be called.
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      loginAlert();
     }
   }
 };
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
+  hideSignUpAlert();
 
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+
+  if(!name || !email || !password || password.length <8) {signUpAlert()}
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
@@ -42,8 +45,6 @@ const signupFormHandler = async (event) => {
   // Again, this page link may need to be changed
     if (response.ok) {
       document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
     }
   }
 };
@@ -55,4 +56,24 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+const signUpAlert = () => {
+  const signUpAlert = document.querySelector('#signUpAlert');
+  signUpAlert.style.display = 'block';
+}
+
+const hideSignUpAlert = () => {
+  const signUpAlert = document.querySelector('#signUpAlert');
+  signUpAlert.style.display = 'none';
+}
+
+const loginAlert = () => {
+  const loginAlert = document.querySelector('#loginAlert');
+  loginAlert.style.display = 'block';
+}
+
+const hideLoginAlert = () => {
+  const loginAlert = document.querySelector('#loginAlert');
+  loginAlert.style.display = 'none';
+}
 
