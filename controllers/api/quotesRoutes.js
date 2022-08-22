db7de99e5c44519dd449c04708447b6f6aa4feb4
dansmith09 <dansmith_09@hotmail.com>
2022-08-22 20:09:21 +1000

    Added random quotes in header functionality

--- controllers/api/quotesRoutes.js
+++ controllers/api/quotesRoutes.js
@@ -70,7 +70,7 @@ router.delete('/:id',  async (req, res) =>  {
         });
 
         if (!quotes) {
-            res.status(404).json({ message: 'No category found with that id!' });
+            res.status(404).json({ message: 'No quote found with that id!' });
             return;
         }
         
